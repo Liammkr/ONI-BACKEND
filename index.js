9fd556a82e5026bb02404024d88b6b5f13f6aa80
Liammkr <liamkr10009@gmail.com>
2024-09-30 18:01:23 -0700

    idk

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
 
+const app = express();
 app.use(express.json());
+
+// Allow all origins permanently
+app.use(cors()); // This will allow requests from any origin
+
 const stripe = require("stripe")(process.env.STRIPEKEY);
-app.use(express.static("public"));
 const YOUR_DOMAIN = "http://oni-nu.vercel.app";
-app.use(cors());
 
 app.post("/checkout", async (req, res) => {
   try {
@@ -34,4 +36,5 @@ app.post("/checkout", async (req, res) => {
 app.get("*", (req, res) => {
   res.redirect("http://oni-nu.vercel.app");
 });
+
 app.listen(3000, () => console.log("Running on port 3000"));
